f6448d44825f26760ad33d9d684da6d09f475dee
Jinyi Xian <kylerky@live.com>
2024-05-09 13:43:07 +0800

    Remove perspective

--- init/workspace.el
+++ init/workspace.el
@@ -8,32 +8,6 @@
   :demand t
   :straight (:type built-in))
 
-(use-package perspective
-  :demand t
-  :bind
-  (("C-x C-b" . (lambda (arg)
-                  (interactive "P")
-                  (if (fboundp 'persp-bs-show)
-                      (persp-bs-show arg)
-                    (bs-show "all"))))
-   ("C-x b" . persp-switch-to-buffer*)
-   ("C-x k" . persp-kill-buffer*))
-  :custom
-  ((persp-mode-prefix-key (kbd "C-c w"))
-   (persp-state-default-file "~/.emacs.d/perspective-state")
-   (even-window-sizes nil))
-  :hook
-  ((kill-emacs . persp-state-save))
-  :init
-  (persp-mode)
-  (unbind-key "C-x 4 b"))
-
-(use-package persp-projectile
-  :after (projectile perspective)
-  :bind
-  (:map projectile-mode-map
-        ("C-c p p" . projectile-persp-switch-project)))
-
 (use-package magit)
 
 (use-package envrc
